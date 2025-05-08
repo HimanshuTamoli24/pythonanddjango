ba150804678c8aeb1cf1747a0da117d214bd0580
Himanshu Tamoli <himanshutamoli2005@gmail.com>
2025-05-08 06:04:59 +0530

    create first model

--- djangolearning/hellotodjango/models.py
+++ djangolearning/hellotodjango/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class ChaiVariety(models.Model):  # Renamed to fix spelling
+    CHAI_TYPE_CHOICE = [
+        ('ML', 'Masala'),
+        ('GR', 'Ginger'),
+        ('BL', 'Black Tea'),
+        ('LI', 'Lemon Tea'),
+    ]
+
+    name = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='chai/')
+    date_added = models.DateField(default=timezone.now)
+    type = models.CharField(choices=CHAI_TYPE_CHOICE, max_length=50)
+
+    def __str__(self):
+        return self.name
--- djangolearning/hellotodjango/views.py
+++ djangolearning/hellotodjango/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-
+from .models import ChaiVariety
 # Create your views here.
 def all_info(req):
     return render(req,'hellotodjango/index.html')
 
 def python(request):
-    return render(request,"hellotodjango/python.html")
+    chais= ChaiVariety.objects.all()
+    return render(request,"hellotodjango/python.html",{'chais':chais})
